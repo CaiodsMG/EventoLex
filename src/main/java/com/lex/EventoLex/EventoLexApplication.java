ab7165391aa974f18c956a9766ac2f0bb76d6597
CaiodsMG <caiocfn10@gmail.com>
2025-09-24 10:50:51 -0300

    Projeto configurado

---
+++ src/main/java/com/lex/EventoLex/EventoLexApplication.java
@@ -0,0 +1,13 @@
+package com.lex.EventoLex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoLexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoLexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lex/EventoLex/EventoLexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex.EventoLex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoLexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
